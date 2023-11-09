7fbc4753ce0991d6f3ae499cddcc6d99544558a2
Josh Dellamuth <joshdellamuth@gmail.com>
2023-11-09 11:54:18 -0600

    Hard-coded data-binding example

--- Maui-Sandbox/Maui-Sandbox/Maui-Sandbox/MainPage.xaml.cs
+++ Maui-Sandbox/Maui-Sandbox/Maui-Sandbox/MainPage.xaml.cs
@@ -1,10 +1,24 @@
-﻿namespace Maui_Sandbox;
+﻿using Maui_Sandbox.ViewModel;
+
+namespace Maui_Sandbox;
 
 public partial class MainPage : ContentPage
 {
 	public MainPage()
 	{
 		InitializeComponent();
+
+		//Creates an instance of the class defined in the viewmodel and
+		//uses it for the binding context (commented out for now)
+		var mainPageViewModel = new MainPageViewModel()
+		{
+			TaskId = "3400",
+			TaskName = "Feed Dog",
+            TaskStatus = "Active",
+            IsActive = true
+        };
+
+		BindingContext = mainPageViewModel;
 	}
 }
 
---
+++ Maui-Sandbox/Maui-Sandbox/Maui-Sandbox/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.ComponentModel;
+
+namespace Maui_Sandbox.ViewModel;
+
+//MVVM can be implemented by implementing INotifyPropertyChanged
+//when we implement the interface (right click when it is underlined red) 
+//implementing the interface just means to include what is in the interface
+//in this case, we implement INotifyPropertyChanged with the public event 
+//line
+public partial class MainPageViewModel : INotifyPropertyChanged
+{
+    public string TaskId { get; set; }
+    public string TaskName { get; set; }
+    public string TaskStatus { get; set; }
+    public bool IsActive { get; set; }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+}
