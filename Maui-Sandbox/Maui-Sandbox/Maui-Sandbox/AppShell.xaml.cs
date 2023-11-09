16a38f479753ae8a52cc34482bad2d45472fa5d4
Josh Dellamuth <joshdellamuth@gmail.com>
2023-11-08 23:59:13 -0600

    Added AppShell.xaml and AppShell.xaml.cs descriptive comments

--- Maui-Sandbox/Maui-Sandbox/Maui-Sandbox/AppShell.xaml.cs
+++ Maui-Sandbox/Maui-Sandbox/Maui-Sandbox/AppShell.xaml.cs
@@ -1,8 +1,12 @@
 ﻿namespace Maui_Sandbox;
 
+//This code initializes 
 public partial class AppShell : Shell
 {
-	public AppShell()
+    // contructor for AppShell that contains InitalizeComponent method and takes no parameters
+    // because it is a constructor, Initialize component is ran when an instance of the class is created
+    // therefore, something like AppShell appShell = new AppShell(); would call the method (also applies to other pages
+    public AppShell()
 	{
 		InitializeComponent();
 	}
